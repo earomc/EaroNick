5d0510b200fec2ee7f2099826decb29480d3d2bd
Tiiita <96084154+Tiiita@users.noreply.github.com>
2022-12-11 02:01:20 +0100

    Added methods to NickManager

--- src/main/java/net/earomc/earonick/manager/NickManager.java
+++ src/main/java/net/earomc/earonick/manager/NickManager.java
@@ -1,10 +1,36 @@
 package net.earomc.earonick.manager;
 
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.plugin.Plugin;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
 /**
  * @author tiiita_
  * Created on Dezember 11, 2022 | 01:49:47
  * (●'◡'●)
  */
 public class NickManager {
-    
+
+    private final HashSet<ProxiedPlayer> nickedPlayers = new HashSet<>();
+    public void nickPlayer(ProxiedPlayer player, String newName) {
+        nickedPlayers.add(player);
+    }
+
+    public void unnickPlayer(ProxiedPlayer player) {
+        nickedPlayers.remove(player);
+    }
+
+    public boolean isNicked(ProxiedPlayer player) {
+        return nickedPlayers.contains(player);
+    }
+
+
+    /*
+    public String getNickName(ProxiedPlayer player) {
+
+    }*/
 }
