742ec235b389189bd0446b72094301c63935a529
Tiiita <96084154+Tiiita@users.noreply.github.com>
2022-12-11 01:42:55 +0100

    Init

---
+++ src/main/java/net/earomc/earonick/EaroNick.java
@@ -0,0 +1,16 @@
+package net.earomc.earonick;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class EaroNick extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
