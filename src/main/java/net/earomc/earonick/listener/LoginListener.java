ddcf743d5520f003c4acf2b1cf805ebbe8c148fa
Tiiita <96084154+Tiiita@users.noreply.github.com>
2022-12-11 01:45:33 +0100

    Added LoginListener.java

---
+++ src/main/java/net/earomc/earonick/listener/LoginListener.java
@@ -0,0 +1,18 @@
+package net.earomc.earonick.listener;
+
+import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
+
+/**
+ * @author tiiita_
+ * Created on Dezember 11, 2022 | 01:44:56
+ * (●'◡'●)
+ */
+public class LoginListener implements Listener {
+
+    @EventHandler
+    public void onLogin(PostLoginEvent event) {
+        
+    }
+}
