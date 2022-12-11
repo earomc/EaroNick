d0ec98b9820e08a9fda6c024644bb9ec7d0d2179
Tiiita <96084154+Tiiita@users.noreply.github.com>
2022-12-11 02:13:08 +0100

    Finished NickCommand

--- src/main/java/net/earomc/earonick/command/NickCommand.java
+++ src/main/java/net/earomc/earonick/command/NickCommand.java
@@ -2,10 +2,14 @@ package net.earomc.earonick.command;
 
 import net.earomc.earonick.EaroNick;
 import net.earomc.earonick.config.ConfigWrapper;
+import net.earomc.earonick.manager.NickManager;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.plugin.Plugin;
+
+import java.util.List;
 
 /**
  * @author tiiita_
@@ -33,9 +37,30 @@ public class NickCommand extends Command {
         ConfigWrapper messageConfig = plugin.getMessageConfig();
 
         if (!player.hasPermission(config.getString("nick-command.permission"))) {
-            player.sendMessage();
+            player.sendMessage(new TextComponent(messageConfig.color(messageConfig.getString("no-permission"))));
             return;
         }
 
+        if (args.length != 1) {
+            sendCommandUsage(player);
+            return;
+        }
+
+        NickManager nickManager = plugin.getNickManager();
+
+        if (nickManager.isNicked(player)) {
+            messageConfig.color(messageConfig.getString("already-nicked"));
+            return;
+        }
+
+        nickManager.nickPlayer(player, args[0]);
+
+    }
+
+    private void sendCommandUsage(ProxiedPlayer player) {
+
+        for (String currentMessage : plugin.getConfig().getStringList("command-usage")) {
+            player.sendMessage(new TextComponent(plugin.getConfig().color(currentMessage)));
+        }
     }
 }
